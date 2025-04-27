f8cbe98d328f7c185e4b6dadaa51e00008d33321
Suraj Kumar <sk96.uk@gmail.com>
2025-04-27 21:19:44 +0100

    feat: update demo to match changes

--- demo/src/commonMain/kotlin/com/wannaverse/countryselector/demo/App.kt
+++ demo/src/commonMain/kotlin/com/wannaverse/countryselector/demo/App.kt
@@ -23,9 +23,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.wannaverse.countryselector.demo.DemoAppTheme
 import com.wannaverse.countryselector.Countries
-import com.wannaverse.countryselector.CountryPickerIcon
+import com.wannaverse.countryselector.CountrySelector
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -65,7 +64,10 @@ fun App() {
                         .padding(8.dp),
                     contentAlignment = Alignment.Center
                 ) {
-                    CountryPickerIcon(selectedCountry) { selectedCountry = it }
+                    CountrySelector(
+                        country = selectedCountry,
+                        onSelection = { selectedCountry = it }
+                    )
                 }
             }
         }

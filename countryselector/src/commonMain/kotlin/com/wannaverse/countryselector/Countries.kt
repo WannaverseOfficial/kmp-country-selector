79ebb9db9738ef10199006805d6bf8c774a86b34
AdiK5050 <kumraditya456@gmail.com>
2026-07-24 17:31:07 +0530

    fix: remove non-country entries EAC and IC, guard invalid regions
    Removes EAC (East African Community) and IC (Canary Islands) from the
    country list. EAC is not an ISO 3166-1 code and caused an
    IllformedLocaleException from Locale.Builder().setRegion() on JVM,
    crashing the app.
    
    Also guards getLocalizedCountryName on JVM so any non-ISO region falls
    back to the raw code instead of crashing, and removes the orphaned
    eac/ic flag drawables.

--- countryselector/src/commonMain/kotlin/com/wannaverse/countryselector/Countries.kt
+++ countryselector/src/commonMain/kotlin/com/wannaverse/countryselector/Countries.kt
@@ -452,13 +452,6 @@ object Countries {
             internationalDialCode = "+213",
             flagImageResource = Res.drawable.dz)
 
-    val EAC : Country
-        get() = Country(
-            countryName = getLocalizedCountryName("EAC", languageTag),
-            countryCode = "EAC",
-            internationalDialCode = "+255",
-            flagImageResource = Res.drawable.eac)
-
     val EC : Country
         get() = Country(
             countryName = getLocalizedCountryName("EC", languageTag),
@@ -724,13 +717,6 @@ object Countries {
             countryCode = "HU",
             internationalDialCode = "+36",
             flagImageResource = Res.drawable.hu)
-
-    val IC : Country
-        get() = Country(
-            countryName = getLocalizedCountryName("IC", languageTag),
-            countryCode = "IC",
-            internationalDialCode = "+34",
-            flagImageResource = Res.drawable.ic)
 
     val ID : Country
         get() = Country(
@@ -1787,9 +1773,9 @@ object Countries {
             AD, AE, AF, AG, AI, AL, AM, AO, AQ, AR,
             AS, AT, AU, AW, AX, AZ, BA, BB, BD, BE, BF, BG, BH, BI, BJ, BL, BM, BN,
             BO, BR, BS, BT, BV, BW, BY, BZ, CA, CC, CD, CF, CG, CH, CI, CK, CL, CM,
-            CN, CO, CR, CU, CV, CW, CX, CY, CZ, DE, DJ, DK, DM, DZ, EAC, EC, EE, EG,
+            CN, CO, CR, CU, CV, CW, CX, CY, CZ, DE, DJ, DK, DM, DZ, EC, EE, EG,
             EH, ER, ES, ET, FI, FJ, FK, FM, FO, FR, GA, GB, GD, GE, GF, GG, GH, GI, GL,
-            GM, GN, GP, GQ, GR, GS, GT, GU, GW, GY, HK, HM, HN, HR, HT, HU, IC, ID, IE,
+            GM, GN, GP, GQ, GR, GS, GT, GU, GW, GY, HK, HM, HN, HR, HT, HU, ID, IE,
             IL, IM, IN, IO, IQ, IR, IS, IT, JE, JM, JO, JP, KE, KG, KH, KI, KM, KN, KP,
             KR, KW, KY, KZ, LA, LB, LC, LI, LK, LR, LS, LT, LU, LV, LY, MA, MC, MD, ME,
             MF, MG, MH, MK, ML, MM, MN, MO, MP, MQ, MR, MS, MT, MU, MV, MW, MX, MY, MZ,
--- countryselector/src/jvmMain/kotlin/com/wannaverse/countryselector/native/CountryLocal.jvm.kt
+++ countryselector/src/jvmMain/kotlin/com/wannaverse/countryselector/native/CountryLocal.jvm.kt
@@ -1,13 +1,19 @@
 package com.wannaverse.countryselector.native
 
+import java.util.IllformedLocaleException
 import java.util.Locale
 
 actual fun getLocalizedCountryName(countryCode: String, localeLanguageTag: String): String {
-    return Locale.Builder()
-        .setLanguageTag(localeLanguageTag)
-        .setRegion(countryCode)
-        .build()
-        .displayCountry
+    return try {
+        val displayCountry = Locale.Builder()
+            .setLanguageTag(localeLanguageTag)
+            .setRegion(countryCode)
+            .build()
+            .displayCountry
+        displayCountry.ifEmpty { countryCode }
+    } catch (e: IllformedLocaleException) {
+        countryCode
+    }
 }
 
 actual fun getLocaleLanguageTag(): String {
